1c91e752be676cdc0ae1df13573d926e73a49898
Misi <61704770+MilanVarady@users.noreply.github.com>
2021-12-19 21:22:06 +0100

    Object dock now carries objects
    Finally!!!

--- Base Level/ObjectDock.gd
+++ Base Level/ObjectDock.gd
@@ -1,9 +1,11 @@
 extends Control
 
+export var objects_path: NodePath
 
 var objects_on_dock: Array = []
 var mouse_inside := false
 var initial_collect := 5
+var area_just_added := false
 
 
 func _ready() -> void:
@@ -33,31 +35,30 @@ func _process(delta: float) -> void:
 
 func _on_DragArea_area_entered(area: Area2D) -> void:
 	if area.is_in_group("Objects") and not objects_on_dock.has(area):
-		if area.drag or initial_collect > 0:
+		if (area.drag or initial_collect > 0) and not objects_on_dock.has(area):
 			area.get_node("Body").scale = Vector2(0.5, 0.5)
-			objects_on_dock.append(area)
 			
-			if initial_collect > 0:
-				area.get_node("HideAnimation").play("Hide")
-
-
-func _on_ExitArea_area_exited(area: Area2D) -> void:
-	if area.is_in_group("Objects") and objects_on_dock.has(area):
-		area.get_node("Body").scale = Vector2(1, 1)
-		
-		objects_on_dock.erase(area)
+			area.get_parent().remove_child(area)
+			$Dock.call_deferred("add_child", area)
+			
+			area_just_added = true
+			
+			objects_on_dock.append(area)
 
 
 func _on_ObjectDock_mouse_entered() -> void:
-	for object in objects_on_dock:
-		object.get_node("HideAnimation").play_backwards("Hide")
-		
 	$Dock/DockAnimation.play("Open")
 
 
-func _on_ObjectDock_mouse_exited() -> void:
-	for object in objects_on_dock:
-		if not object.drag:
-			object.get_node("HideAnimation").play("Hide")
-	
+func _on_ObjectDock_mouse_exited() -> void:	
 	$Dock/DockAnimation.play_backwards("Open")
+
+
+func _on_DragArea_area_exited(area: Area2D) -> void:
+	if area.is_in_group("Objects") and objects_on_dock.has(area):
+		area.get_parent().call_deferred("remove_child", area)
+		get_node(objects_path).call_deferred("add_child", area)
+		
+		area.get_node("Body").scale = Vector2.ONE
+		
+		objects_on_dock.erase(area)
