4cda474c6e41597af8d3aaad3fdd0da2958420f3
Misi <61704770+MilanVarady@users.noreply.github.com>
2021-12-24 10:29:21 +0100

    1.0 release

--- Level Select/Credits.gd
+++ Level Select/Credits.gd
@@ -2,7 +2,7 @@ extends Control
 
 
 func _on_MusicLink_pressed() -> void:
-	OS.shell_open("https://www.context-sensitive.com/")
+	OS.shell_open("https://www.youtube.com/channel/UCT1ZkP03V18LmOj8zbyP-Dw?")
 
 
 func _on_Button_pressed() -> void:
